1c5aa6a6cba0b70393230c7cfe9beaf0c6b60408
hexlianine <1669264+hexlianine@users.noreply.github.com>
2026-05-05 16:07:06 +0800

    feat: add the verify plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig } from "hardhat/config";
 import dotenv from "dotenv";
 import hardhatEthers from "@nomicfoundation/hardhat-ethers";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 
 dotenv.config();
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 export default defineConfig({
-  plugins: [hardhatEthers],
+  plugins: [hardhatEthers, hardhatVerify],
   solidity: {
     version: "0.8.28",
   },
   networks: {
     sepolia: {
       type: "http",
-      url: API_URL || "",
-      accounts: [`0x${PRIVATE_KEY}`],
+      url: API_URL!,
+      accounts: [`0x${PRIVATE_KEY!}`],
     }
-  }
+  },
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY!,
+    }
+  },
 });
