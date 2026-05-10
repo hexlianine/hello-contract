21c4636ff847a39e755d9f29eeb64bf7d2c8ef44
hexlianine <1669264+hexlianine@users.noreply.github.com>
2026-05-10 20:37:14 +0800

    refactor: create client with chain id

--- app/eth/client.ts
+++ app/eth/client.ts
@@ -1,16 +1,27 @@
-import { createPublicClient, createWalletClient, webSocket } from 'viem';
-import { sepolia } from 'viem/chains';
+import { createPublicClient, createWalletClient, Transport, webSocket } from 'viem';
+import * as chains from 'viem/chains';
 
 const API_URL = process.env.APP_API_URL;
 
-export const publicClient = createPublicClient({
-  chain: sepolia,
-  transport: webSocket(`${API_URL}`),
-});
+const transport = webSocket(`${API_URL}`);
 
-export const walletClient = createWalletClient({
-  chain: sepolia,
-  transport: webSocket(`${API_URL}`),
-});
+export const publicClient = generate(chains.sepolia.id, false, transport);
+export const walletClient = generate(chains.sepolia.id, true, transport);
 
+function generate(chainId: number, wallet: boolean, transport: Transport) {
+  const chain = Object.values(chains).find((c) => c.id === chainId);
 
+  if (!chain) {
+    throw new Error(`Chain ${chainId} not found`);
+  }
+
+  return wallet
+    ? createWalletClient({
+        chain,
+        transport: transport,
+      })
+    : createPublicClient({
+        chain,
+        transport: transport,
+      });
+}
